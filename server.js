6a27c14e67ea667131d4725f8e219f47f0cb8ede
Tamir <1tamir198@gmail.com>
2023-03-16 18:43:30 +0200

    Changing starter files

--- server.js
+++ server.js
@@ -1,96 +1,66 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+const data = [
+  {
+    id: "0",
+    driver: "Ed shiran",
+    passenger: "Shaul Shauli",
+    from: "Ben Zion St.12 Tel Aviv",
+    to: "Weizmann St. 6 Ramat Gan",
+    status: "Pending",
+  },
 
-const express = require('express')
-const app = express()
-var bodyParser = require('body-parser')
-const port = 2500
+  {
+    id: "1",
+    driver: "Miky Jannet",
+    passenger: "Simcha Stern",
+    from: "Ben Zion St.12 Tel Aviv",
+    to: "Weizmann St. 6 Ramat Gan",
+    status: "Rolling",
+  },
 
-app.use(bodyParser.json)
+  {
+    id: "2",
+    driver: "Tamir Abutbul",
+    passenger: "Bar Cohen",
+    from: "Ben Zion St.12 Tel Aviv",
+    to: "Weizmann St. 6 Ramat Gan",
+    status: "Complete",
+  },
 
-const data = 
-[
-    {
-        id:"0",
-        driver:"Ed shiran",
-        passenger: "Shaul Shauli",
-        from: "Ben Zion St.12 Tel Aviv",
-        to: "Weizmann St. 6 Ramat Gan",
-        status: "Pending"
-    },
+  {
+    id: "3",
+    driver: "Adi Unger",
+    passenger: "Shaul Hanavi",
+    from: "Ben Zion St.12 Tel Aviv",
+    to: "Weizmann St. 6 Ramat Gan",
+    status: "Pending",
+  },
 
-    {
-        id:"1",
-        driver:"Miky Jannet",
-        passenger: "Simcha Stern",
-        from: "Ben Zion St.12 Tel Aviv",
-        to: "Weizmann St. 6 Ramat Gan",
-        status: "Rolling"
+  {
+    id: "4",
+    driver: "Yuval David",
+    passenger: "Israel Israeli ",
+    from: "Ben Zion St.12 Tel Aviv",
+    to: "Weizmann St. 6 Ramat Gan",
+    status: "Rolling",
+  },
 
-    },
+  {
+    id: "5",
+    driver: "Noa Kirel",
+    passenger: "Yona Shauli",
+    from: "Ben Zion St.12 Tel Aviv",
+    to: "Weizmann St. 6 Ramat Gan",
+    status: "Complete",
+  },
+];
 
-    {
-        id:"2",
-        driver:"Tamir Abutbul",
-        passenger: "Bar Cohen",
-        from: "Ben Zion St.12 Tel Aviv",
-        to: "Weizmann St. 6 Ramat Gan",
-        status: "Complete"
-    },
-
-    {
-        id:"3",
-        driver:"Adi Unger",
-        passenger: "Shaul Hanavi",
-        from: "Ben Zion St.12 Tel Aviv",
-        to: "Weizmann St. 6 Ramat Gan",
-        status: "Pending"
-    },
-
-    {
-        id:"4",
-        driver:"Yuval David",
-        passenger: "Israel Israeli ",
-        from: "Ben Zion St.12 Tel Aviv",
-        to: "Weizmann St. 6 Ramat Gan",
-        status: "Rolling"
-    },
-
-    
-    {
-        id:"5",
-        driver:"Noa Kirel",
-        passenger: "Yona Shauli",
-        from: "Ben Zion St.12 Tel Aviv",
-        to: "Weizmann St. 6 Ramat Gan",
-        status: "Complete"
-    }
-]
-
-
-
-function updateStatus(obj) {
-    const index = data.findIndex(object =>  object.id === obj.id )
-    return index  
-}
-
-  
-
-app.post('/update-status', (req, res) => {
-    req.send( 
-        {
-            id:"5",
-            driver:"Noa Kirel",
-            passenger: "Yona Shauli",
-            from: "Ben Zion St.12 Tel Aviv",
-            to: "Weizmann St. 6 Ramat Gan",
-            status: "Complete"
-        })
-    
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post("/update-status", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
